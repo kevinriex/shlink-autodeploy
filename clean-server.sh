f94b6b809537c132be54e085f887044e5b9c42c6
Kevin Yven Riexinger <github@me.kevinriex.de>
2024-04-04 23:00:07 +0200

    feat: added docker / docker-compose install

--- clean-server.sh
+++ clean-server.sh
@@ -3,6 +3,18 @@
 # variables
 username=dude
 
+# remove packages
+apt-get purge pwgen ca-certificates -y 
+apt-get autoremove -y
+
+# remove docker-ce
+apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+rm -rf /etc/apt/sources.list.d/docker.list
+rm -rf /etc/apt/keyrings/docker.asc
+
+# remove docker-compose
+rm -rf /user/local/bin/docker-compose
+
 # remove user
 /sbin/userdel ${username}
 rm -rf /home/dude
--- shlink-autodeploy.sh
+++ shlink-autodeploy.sh
@@ -4,12 +4,42 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install nano curl sudo pwgen
+apt-get install nano curl sudo pwgen ca-certificates -y
+
 
 # 2. add user
+
 username="dude"
 userpasswd=$(pwgen -y -c -n -s 24 1)
+
 /sbin/useradd -m -p $(openssl passwd -1 $userpasswd) -s /bin/bash ${username} 
+/sbin/usermod -aG sudo $username
+
+
+# 3. install docker-cd
+# docs: https://docs.docker.com/engine/install/debian/
+
+# gpg key
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# add source-repo to sources.list
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# install
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+
 
 # n-1 print passwd for user
 echo "The new user is " $username "identified by: ${userpasswd}"
