6429fb5707aa266b29c0788861f58cd884bc850d
Kevin Yven Riexinger <github@me.kevinriex.de>
2024-04-04 22:43:49 +0200

    fix: added hash to passwd

--- shlink-autodeploy.sh
+++ shlink-autodeploy.sh
@@ -7,9 +7,9 @@ apt-get upgrade -y
 apt-get install nano curl sudo pwgen
 
 # 2. add user
-username=dude
+username="dude"
 userpasswd=$(pwgen -y -c -n -s 24 1)
-/sbin/useradd -m -p '${userpasswd}' ${username} /bin/bash
+/sbin/useradd -m -p $(openssl passwd -1 $userpasswd) -s /bin/bash ${username} 
 
 # n-1 print passwd for user
-echo "The new user is " ${username} "identified by: ${userpasswd}"
+echo "The new user is " $username "identified by: ${userpasswd}"
