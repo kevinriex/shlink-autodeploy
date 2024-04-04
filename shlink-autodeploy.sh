c9b4ef982db1c10d3301549fd49f6d3f94065d41
Kevin Yven Riexinger <github@me.kevinriex.de>
2024-04-04 22:31:10 +0200

    fix: userpwd in ""

--- shlink-autodeploy.sh
+++ shlink-autodeploy.sh
@@ -9,7 +9,7 @@ apt-get install nano curl sudo pwgen
 # 2. add user
 username=dude
 userpasswd=$(pwgen -y -c -n -s 24 1)
-/sbin/useradd -m -p ${userpasswd} ${username} /bin/bash
+/sbin/useradd -m -p '${userpasswd}' ${username} /bin/bash
 
 # n-1 print passwd for user
 echo "The new user is " ${username} "identified by: ${userpasswd}"
